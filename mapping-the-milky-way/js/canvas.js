75ac712070c9b8aed35721fac6d154081c0bd4ef
Abbey Yacoe <ayacoe@lsst.org>
2019-02-05 14:00:20 -0700

    Position Legend

--- mapping-the-milky-way/js/canvas.js
+++ mapping-the-milky-way/js/canvas.js
@@ -110,7 +110,6 @@ function render() {
   renderer.render( scene, camera );
 
   renderer.clearDepth(); // important! clear the depth buffer
-  renderer.setViewport( window.innerWidth - 350 , window.innerHeight -325, 400, 230 );
-  // renderer.setViewport( 0,0, window.innerWidth, window.innerHeight);
+  renderer.setViewport( window.innerWidth - 350 , window.innerHeight -225, 400, 230 );
   renderer.render( scene2, camera2 );
 }
